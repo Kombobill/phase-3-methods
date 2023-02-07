c213711f06dc85e66807134de727c81823eb2c7b
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-02-08 00:02:16 +0300

    done
    done with assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+
+def greet_programmer 
+    puts  "Hello, programmer!"
+  end  
+  greet_programmer
+  
+  
+  
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  greet("Naureen")
+  
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  greet_with_default "Naureen"
+  
+  
+  def add(x, y)
+    return x + y
+  end
+  
+  
+  def halve(x)
+    if x.is_a?(Integer)
+     return x / 2
+    else
+     return nil
+    end
+  end
+  
